f159d2115f782a83e25bf14927bdee2a766c4729
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-12 15:10:49 +0100

    fix no fetching url

--- src/utils/helpers/firebase.helper.js
+++ src/utils/helpers/firebase.helper.js
@@ -1,4 +1,6 @@
-const createToast = (title, description, status) => ({
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
+export const createToast = (title, description, status) => ({
   title,
   description,
   status,
@@ -8,10 +10,26 @@ const createToast = (title, description, status) => ({
 });
 
 export const firebaseErrorToast = (error) => {
-   
-  return createToast("Error", error.message.match(/auth\/(.*)\)/)[1];, "error");
+  return createToast("Error", error.message.match(/auth\/(.*)\)/)[1], "error");
 };
 
 export const firebaseSuccessToast = (message) => {
   return createToast("Success", message, "success");
+};
+
+export const fetchProfileImageUrl = async (userEmail) => {
+  const storage = getStorage();
+  const imagePath = `/users/${userEmail}/profile/profile.png`;
+  const imageRef = ref(storage, imagePath);
+
+  try {
+    const url = await getDownloadURL(imageRef);
+    return url;
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération de l'URL de l'image :",
+      error
+    );
+    // Gérer l'erreur, par exemple, retourner une URL d'image par défaut si nécessaire
+  }
 };
