ed28df571981a00a5eb00ef0ea0281f2b9785c2b
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-12 15:11:16 +0100

    use localArea network ip

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // send .com or all all all url to index.html
+  server: {
+    fs: {
+      strict: false,
+    },
+    host: "0.0.0.0",
+  },
+});
