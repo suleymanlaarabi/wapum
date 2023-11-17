8c18e38aa002dafbfa111cda790c27d205ad3492
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-18 00:52:39 +0100

    fix more than 13 message to fetch

--- src/hooks/useMessages.jsx
+++ src/hooks/useMessages.jsx
@@ -12,11 +12,9 @@ import useRealtimeFirestore from "./useRealtimeFirestore";
 import { db } from "../../firebase.config";
 import useFirestore from "./useFirestore";
 import useAuth from "./useAuth";
-import { useState } from "react";
 
 const useMessages = (conversationId) => {
   const { currentUser } = useAuth();
-  const [messageLoaded, setMessageLoaded] = useState(13); // 2 messages par batch
   const { documents, addDataToDocuments } = useRealtimeFirestore(
     "messages",
 
@@ -24,12 +22,22 @@ const useMessages = (conversationId) => {
       collection(db, "messages"),
       where("conversationId", "==", conversationId),
       orderBy("timestamp", "desc"),
-      limit(messageLoaded)
+      limit(13)
     ),
     ["conversations", conversationId]
   );
   const { createDocument } = useFirestore("messages");
   const sendMessage = (message) => {
+    addDataToDocuments([
+      ...documents,
+      {
+        id: 11090110901,
+        conversationId,
+        message,
+        sender: currentUser.email,
+        timestamp: new Date(),
+      },
+    ]);
     createDocument({
       conversationId,
       message,
@@ -46,10 +54,9 @@ const useMessages = (conversationId) => {
         where("conversationId", "==", conversationId),
         orderBy("timestamp", "desc"),
         startAfter(lastMessage.timestamp), // Utilise startAfter avec le dernier message de la première batch
-        limit(messageLoaded)
+        limit(13)
       )
     );
-    await setMessageLoaded((current) => current + messageLoaded);
     await addDataToDocuments(
       docs.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
     );
