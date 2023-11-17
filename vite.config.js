3501f985c09cb28f69c69aebce1eaf4ae962e7f6
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-18 00:53:47 +0100

    add wasm plugin

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
+import wasmPack from "vite-plugin-wasm-pack";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), wasmPack("./wapum-rust")],
   // send .com or all all all url to index.html
   server: {
     fs: {
