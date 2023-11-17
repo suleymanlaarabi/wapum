790bdb26e3e6d8d6b87346463ccb8321cfcbd968
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-18 00:50:12 +0100

    remove unused

--- src/components/Chat/ChatConversation.jsx
+++ src/components/Chat/ChatConversation.jsx
@@ -8,56 +8,9 @@ import dayjs from "dayjs";
 
 const ChatConversation = ({ messages, loadMore }) => {
   const { currentUser } = useAuth();
-  const messageBoxRef = useRef();
-  const handleScroll = async () => {
-    const scrollTop = messageBoxRef.current.scrollTop;
-    if (scrollTop === 0) {
-      await loadMore();
-    }
-  };
-
-  useEffect(() => {
-    // Ajouter le gestionnaire d'événements lors du montage du composant
-    if (messageBoxRef.current) {
-      messageBoxRef.current.addEventListener("scroll", handleScroll);
-
-      // Nettoyer le gestionnaire d'événements lors du démontage du composant
-      return () => {
-        messageBoxRef.current?.removeEventListener("scroll", handleScroll);
-      };
-    }
-  }, [loadMore]);
-
-  useEffect(() => {
-    // Faire défiler la boîte de messages vers le bas lorsque de nouveaux messages sont chargés
-    messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
-  }, [messages]);
+  
   return (
-    <VStack
-      height="calc(100vh - 200px)"
-      overflowY={"auto"}
-      spacing={4}
-      width="full"
-      padding={5}
-      alignItems="stretch"
-      ref={messageBoxRef}
-    >
-      {messages.map((msg, index) => {
-        let isDestinataire = false;
-        if (msg.sender === currentUser.email) {
-          isDestinataire = true;
-        }
-        const date = dayjs(msg.timestamp).format("HH:mm");
-        return (
-          <ChatMessage
-            key={index}
-            message={msg.message}
-            isDestinataire={!isDestinataire}
-            date={date ? date : "00:00"}
-          />
-        );
-      })}
-    </VStack>
+    
   );
 };
 
